f896b0c4c67fafb70de78b01ff6cc36d81ba1e8f
joshuacoleophs <71695876+joshuacoleophs@users.noreply.github.com>
2020-09-30 21:18:01 +0100

    Add files via upload

---
+++ password genorator.py
@@ -0,0 +1,10 @@
+# password maker 
+
+activity = str(input("enter an activity you like to do >>> ")).lower()
+number = int(input("enter your favourite number >>> "))
+randomObject = str(input("enter a randon object >>> ")).upper()
+randomInstrument = str(input("enter a random instrument >>>" )).lower()
+specialCharater = input("enter a special charater >>> ")
+specialCharacter2 = input("enter another special charater >>> ")
+number2 = int(input("enter a random number >>> "))
+print(activity[3:5],specialCharater,randomObject[-1],number2,randomInstrument[0:3],specialCharacter2,number)
